0f6a57e4dac6fda2a8f636dd94548deb52afacb7
Abhay Sharma <sharma.abhay2626@gmail.com>
2020-07-08 12:17:17 +0530

    ref to another project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'tree-traversal';
   jsonData: any;
+  isOneSelected: boolean;
+  clickedClick: boolean;
   // receivedData: any;
 
   ngOnInit() {
